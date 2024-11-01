9ee8f8cfcd45bf8dc334d8c941a7699080bcc66e
Bobby Tables <108997136+Vikhyatz@users.noreply.github.com>
2024-11-01 13:32:13 +0530

    Delete nextjs-dashboard directory

--- nextjs-dashboard/app/ui/fonts.tsx
+++
@@ -1,4 +0,0 @@
-import { Lusitana, Poppins  } from 'next/font/google';
-
-export const lusitana = Lusitana({ weight: '400' , subsets: ['latin'] });
-export const poppins = Poppins({ weight: '400' , subsets: ['latin'] });
